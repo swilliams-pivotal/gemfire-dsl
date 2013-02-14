e474dba4331eaf58d5189676bc755dfaa8caa75b
Pid <pid@pidster.com>
2013-02-14 09:34:21 +0000

    remove old impl

--- src/main/groovy/org/tpi/pso/gemfire/dsl/builder/RegionDiskDelegate.groovy
+++
@@ -1,9 +0,0 @@
-package org.tpi.pso.gemfire.dsl.builder
-
-
-class RegionDiskDelegate {
-
-    def methodMissing(String name, map) {
-        println "RegionDiskDelegate.missing.$name($map)"
-    }
-}
